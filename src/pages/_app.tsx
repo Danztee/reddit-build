ed9d369f7959cda2d876f07cd444d2e3491d12e1
Olowoniyi Daniel <101122783+Danztee@users.noreply.github.com>
2023-02-16 19:58:16 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,12 @@ export default function App({ Component, pageProps }: AppProps) {
           content="Reddit web application created by Olowoniyi Daniel"
         />
         <link
+          rel="favicon"
+          type="image/png"
+          sizes="96x96"
+          href="https://www.redditstatic.com/desktop2x/img/favicon/favicon-96x96.png"
+        />
+        <link
           rel="icon"
           type="image/png"
           sizes="96x96"
