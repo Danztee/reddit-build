303f8ce04a3b6292ea816a1ccbaff8e1ce9c348d
Olowoniyi Daniel <101122783+Danztee@users.noreply.github.com>
2023-02-17 02:48:20 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Reddit Build</title>
+        <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <meta
